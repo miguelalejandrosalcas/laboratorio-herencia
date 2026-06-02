87203ab0b20aa7597a86b8002364dabb5d2fec10
Miguel Alejandro <miguelalejandrosalcas@gmail.com>
2026-06-02 01:08:39 -0500

    Archivo actualizado

--- ejercicio1.js
+++ ejercicio1.js
@@ -25,8 +25,12 @@ class Empleado {
     }
 }
 
-let empleado1 = new Empleado("Alejandro", "Director gerencial", 10000000);
-let empleado2 = new Empleado("Carlos", "Desarrollador Web", 10000000);
+let empleado1 = new Empleado("Alejandro", "Director gerencial", 11000000);
+let empleado2 = new Empleado("Carlos", "Desarrollador Web", 8000000);
+let empleado3 = new Empleado("Gabriel", "Ciberseguridad", 15000000);
+let empleado4 = new Empleado("Juan", "Ejecutivo comercial", 9000000);
+let empleado5 = new Empleado("Maria", "Jefe de proyectos", 6500000);
+let empleado6 = new Empleado("Camila", "Analista de datos", 7000000);
 // console.log(empleado1);
 // console.log(empleado2);
 // console.log(empleado1.presentarse());
@@ -38,22 +42,26 @@ console.log(empleado2.calcularSalarioAnual());
 
 
 
-class Gerente extends Empleado {
-    constructor(nombre, cargo, salario, departamento) {
-        super(nombre, cargo, salario);
-        this.departamento = departamento;
+class Lider extends Empleado {
+    constructor(nombre, salario, equipo) {
+        super(nombre, cargo = "Líder del equipo", salario);
+        this.equipo = [];
     }
-    infoGerente() {
-        return `
-        Nombre: ${this.nombre}
-        Cargo: ${this.cargo}
-        Salario mensual: $${this.salario}
-        Salario anual: $${this.salario * 12}
-        Activo: ${this.activo ? "Sí" : "No"}
-        Departamento: ${this.departamento}
-        `;
-    }   
+    presentarEquipo() {
+        if (this.equipo.length > 0) {
+            for (let i = 0; i < this.equipo.length; i++) {
+                this.equipo.push()
+            }
+        }
+        else {
+            return `El equipo del líder está vacío`
+        }
+    }
+    agregarMiembro(nombre) {
+
+    }
+    info() {
+
+    }
 }
 
-let gerente1 = new Gerente("María", "Gerente de Marketing", 8000000, "Marketing");
-console.log(gerente1.infoGerente());
