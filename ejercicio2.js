d058afa10fd3448862f7d3f84d4af4967beab157
Miguel Alejandro <miguelalejandrosalcas@gmail.com>
2026-06-02 01:08:22 -0500

    Archivo actualizado

--- ejercicio2.js
+++ ejercicio2.js
@@ -63,20 +63,31 @@ class Serie extends Contenido {
             ${super.ficha()}
             Temporadas: ${this.temporadas}
             Total episodios: ${this.totalEpisodios()}
-        
+
         `;
 
     }
 }
 
-const peli1 = new Pelicula("El señor de los cuchillos", "Acción", 2020, 130);
-console.log(peli1.duracionFormateada());
+const peli1 = new Pelicula("El señor de los cuchillos", "Acción", 2020, 250);
+const peli2 = new Pelicula("Avatar", "Acción", 2010, 220);
+const peli3 = new Pelicula("Sherlok Holmes", "Acción", 2022, 180);
+peli1.disponible = false;
+peli3.disponible = false;
+
 
 const serie1 = new Serie("Malcolm in the Middle", "Comedia", 2000, 10);
-console.log(serie1.registrarEpisodios(20));
-console.log(serie1.ficha());
+const serie2 = new Serie("Naruto", "Acción", 2000, 50);
+const serie3 = new Serie("Invincible", "Acción", 2026, 20);
+serie1.disponible = false;
+// console.log(serie1.registrarEpisodios(20));
+// console.log(serie1.ficha());
+// console.log(peli1.duracionFormateada());
+// console.log(serie1.estado());
 
-console.log(serie1.estado());
+const catalogo = [peli1, peli2, peli3, serie1, serie2, serie3];
 
-
+for (let i = 0; i < catalogo.length; i++) {
+    console.log(catalogo[i].estado());
+}
 
